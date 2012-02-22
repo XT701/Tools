dd13104695f6d918208d8ba0567b40cb639c2e29
stlifey <stlifey@gmail.com>
2012-02-22 08:51:27 +0800

    Boot/Recovery Repack tool

---
+++ Kernel.mk
@@ -0,0 +1,50 @@
+#!/bin/bash
+PS3="Choose (1-2):"
+echo "Choose your kernel from the list below."
+select kernel in Kernel Kernel_DEPRECATED
+do
+	break
+done
+PS3="Choose (1-2):"
+echo "Choose your ramdisk from the list below."
+select name in CyanogenMod7 CyanogenMod9 OpenRecovery
+do
+	break
+done
+
+pack()
+{
+./repack-bootimg.pl zImage $name/ boot.img
+rm ./OTA/boot.img
+cp ./boot.img ./OTA/
+cd ./OTA/
+zip -r -9 ../Sign/update.zip ./
+cd ../Sign/
+java -Xmx512m -jar signapk.jar -w testkey.x509.pem testkey.pk8 update.zip ../#$(cat $(pwd)/../../$kernel/.version)-$name-$(stat -c %y $(pwd)/../../$kernel/arch/arm/boot/zImage | cut -b1-10).zip
+rm update.zip
+echo "Pack boot OTA package done!"
+}
+
+if [ $name = "OpenRecovery" ]; then
+cp $(pwd)/../$kernel/arch/arm/boot/zImage ./
+rm ./#*.img
+./repack-bootimg.pl zImage ../OpenRecovery/lite/ramdisk recovery.img
+echo "Pack recovery image done!"
+else
+PS3="Choose (Yes/No):"
+echo "Build OTA package?"
+select bool in No Yes
+do
+	break
+done
+echo "You chose RAMDISK=$name,Kernel=$kernel,VERSION=$(cat $(pwd)/../$kernel/.version)"
+cp $(pwd)/../$kernel/arch/arm/boot/zImage ./
+rm ./#*.img
+./repack-bootimg.pl zImage $name/ \#$(cat $(pwd)/../$kernel/.version)-$name-$(stat -c %y $(pwd)/../$kernel/arch/arm/boot/zImage | cut -b1-10).img
+echo "Pack boot image done!"
+if [ $bool = "Yes" ]; then
+pack
+else
+exit 0
+fi
+fi
